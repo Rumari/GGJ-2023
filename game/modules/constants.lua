a2208409181cc5c61bcc9a9c99d7c38c8222eb23
RiscadoA <me@riscadoa.com>
2023-02-04 17:47:39 +0000

    add attack coordination

--- game/modules/constants.lua
+++ game/modules/constants.lua
@@ -6,10 +6,13 @@ DOWN = hash("down")
 
 -- messages
 PUNCH = hash("punch")
+CAN_ATTACK = hash("can_attack")
+DIED = hash("died")
+SET_STANCE = hash("set_stance")
 
 -- enemy constants
 ENEMY_ATTACK_RANGE = 2
-ENEMY_DISTANCE_RANGE = 6
+ENEMY_DISTANCE_RANGE = 4
 
 ENEMY_DISTANCE = 0
 ENEMY_FIGHT = 1
